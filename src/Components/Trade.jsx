baaa5cf98e518ae703a61b47a80fb9f29d9defc0
izima01 <kingsleyizima@gmail.com>
2022-09-09 03:55:34 +0100

    more commits

--- src/Components/Trade.jsx
+++ src/Components/Trade.jsx
@@ -6,7 +6,7 @@ import eth from '../Assets/ethereum.svg';
 
 const Trade = () => {
   return (
-    <div className='bg-white px-4 xl:px-16 lg:px-11 md:px-7 mt-48'>
+    <div className='bg-white px-4 xl:px-16 lg:px-11 md:px-7 mt-56 sm:mt-48'>
         <h2 className='text-black text-[2.5rem] px-4 sm:px-10 lg:px-36 xl:px-60 font-bold text-center mb-16'>Trade securely and market the high growth cryptocurrencies.</h2>
         <div className='sm:flex-row w-10/12 mx-auto sm:w-full flex-col flex gap-3 md:gap-5 lg:gap-9 mb-8'>
             <Crypto
